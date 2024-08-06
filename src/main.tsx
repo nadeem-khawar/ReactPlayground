8020a518431cc0e0954e48662a1d8d1d75aec786
Nadeem Khawar <nadeem.khawar@hotmail.com>
2024-08-06 14:10:36 +0500

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './main.css';
+
+const browserRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Hello World</div>,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={browserRouter} />
+    </React.StrictMode>,
+);
